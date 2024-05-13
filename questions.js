b1892aef93a4a295f3159be8984bd8d898264ef8
ismailaras3 <116161107+ismailaras3@users.noreply.github.com>
2024-05-13 21:17:59 +0200

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,115 @@
+
+let questions = [
+    {
+    numb: 1,
+    question: "Welk van deze verpakkingen is het meest milieuvriendelijke?",
+    antwoord: "Herbruikbare fles",
+    options: [
+      "Plastic fles",
+      "Kartonnen doos",
+      "Herbruikbare fles",
+      "Glas"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Wanneer ontstond het bedrijf?",
+    antwoord: "1994",
+    options: [
+      "1880t",
+      "1974",
+      "1994",
+      "2001"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Wat was de eerste naam van het bedrijf?",
+    antwoord: "Euro-Household",
+    options: [
+      "Cooker",
+      "Euro-Household",
+      "kookade",
+      "Berghoff"
+    ]
+  },
+    {
+    numb: 4,
+    question: "In hoeveel landen is het bedrijf te vinden?",
+    antwoord: "+60",
+    options: [
+      "+700",
+      "+150",
+      "+60",
+      "+20"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Hoe zou u krassen kunnen voorkomen op een inductieplaat?",
+    antwoord: "Door de pan even op te tillen",
+    options: [
+      "Door de pan even op te tillen",
+      "Door de pan stevig langs de onderkant schoonmaken",
+      "Door een onderlegger onder de pan te zetten",
+      "Door geen pan te gebruiken"
+    ]
+  },
+  
+
+   {
+    numb: 6,
+    question: "Wat zou een geschikt schoonmaakmiddel zijn om de kookplaat schoon te maken?",
+    antwoord: "Een microvezeldoekje",
+    options: [
+      "Een schuursponsje",
+      "Een microvezeldoekje",
+     "Het gebruik maken van Cif",
+     "Soda"
+     ]
+  },
+  {
+    numb: 7,
+    question: "Wat zou je moeten gebruiken als er etensresten aan de kookplaat hangen?",
+    antwoord: "Een kookplaatkrabber",
+    options: [
+      "Een schuursponsje",
+      "Een scrub borstel",
+     "Het gebruik maken van Cif",
+     "Een kookplaatkrabber"
+     ]
+  },
+  {
+    numb: 8,
+    question: "Hoe voorkom je kromgetrokken pannen?",
+    antwoord: "Door de pan op middelhoog tot hoog vuur te verwarmen met ingrediënten in",
+    options: [
+      "Door de pan leeg voor te verwarmen op vol vermogen",
+      "Door de pan op te warmen in de microgolf",
+     "Door de pan op middelhoog tot hoog vuur te verwarmen met ingrediënten in",
+     "Door de pan in de vriezer te plaatsen voordat je hem gebruikt"
+     ]
+  },
+  {
+    numb: 9,
+    question: "Hoe ga je duurzaam aan de slag in je keuken?",
+    antwoord: "juist antwoord of the question is here",
+    options: [
+      "Een schuursponsje",
+      "Een microvezeldoekje",
+     "Het gebruik maken van Cif",
+     "Soda"
+     ]
+  },
+  {
+    numb: 10,
+    question: "Waar is het hoofdkantoor van BergHOFF gevestigd",
+    antwoord: "juist antwoord of the question is here",
+    options: [
+      "Brugge, België",
+      "Berlijn, Duitsland",
+     " New York City, Verenigde Staten",
+     "Gent , België"
+     ]
+  },
+];
